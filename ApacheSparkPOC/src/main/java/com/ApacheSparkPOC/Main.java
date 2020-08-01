317dab133ca6c19e04c41a92dd19fd0837e1137d
tharopan <tharopan\@gmail.com>
2020-08-01 10:28:29 +0100

    First commit apache spark POC

---
+++ ApacheSparkPOC/src/main/java/com/ApacheSparkPOC/Main.java
@@ -0,0 +1,32 @@
+package com.ApacheSparkPOC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Double> inputData = new ArrayList<Double>();
+		inputData.add(3.5);
+		inputData.add(4.56);
+		inputData.add(4444.33);
+		
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+		
+		SparkConf conf = new SparkConf();
+		conf.setAppName("ApacheSparkPOC").setMaster("local[*]");
+		
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD javaRDD = sc.parallelize(inputData);
+		
+		sc.close();
+	}
+
+}
