845ad06d80eae42b578709395c2568da4848ce2d
tharopan <tharopan\@gmail.com>
2020-08-04 21:38:27 +0100

    Added some fuinctions

--- ApacheSparkPOC/src/main/java/com/ApacheSparkPOC/ClusterManager.java
+++ ApacheSparkPOC/src/main/java/com/ApacheSparkPOC/ClusterManager.java
@@ -2,28 +2,27 @@ package com.ApacheSparkPOC;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.ml.*;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class ClusterManager {
 	
-	public int findClusters(SparkSession spark, MLibAlgorithm algorithm , String datasetUrl) {
-		
+	public int findClusters(SparkSession spark, MLibAlgorithm algorithm , String datasetUrl) {		
 		switch(algorithm) {
-		  case MLibAlgorithm.KMeans:
+		  case KMeans:
 		    return kMeans(spark, datasetUrl);
-		    break;
-		  case MLibAlgorithm.GMMs:
+		    //break;
+		  case GMMs:
 		    return GMMs(spark, datasetUrl);
-		    break;
+		    //break;
 		  default:
+			  return 0;
 		    // code block
 		}
 	}
 	
-	public int kMeans(SparkSession spark, String datasetUrl)	{
+	public int kMeans(SparkSession spark, String datasetUrl) {
 		String algorithm = "libsvm";
 		datasetUrl = "data/mllib/sample_kmeans_data.txt";
 		
@@ -35,7 +34,7 @@ public class ClusterManager {
 		Dataset<Row> dataset = spark.read().format(algorithm).load(datasetUrl);
 		
 		// Trains a k-means model.
-		KMeans kmeans = new KMeans().setK(2).setSeed(1L);
+		/*KMeans kmeans = new KMeans().setK(2).setSeed(1L);
 		KMeansModel model = kmeans.fit(dataset);
 
 		// Make predictions
@@ -50,8 +49,8 @@ public class ClusterManager {
 		// Shows the result.
 		Vector[] centers = model.clusterCenters();
 		
-		return centers.count();/**/
-		//return 1;
+		return centers.count();*/
+		return 1;
 	}
 
 	public int GMMs(SparkSession spark, String datasetUrl) {
@@ -63,9 +62,9 @@ public class ClusterManager {
 		Dataset<Row> dataset = spark.read().format(format).load(datasetUrl);
 
 		// Trains a GaussianMixture model
-		GaussianMixture gmm = new GaussianMixture().setK(2);
+	//	GaussianMixture gmm = new GaussianMixture().setK(2);
 		
-		GaussianMixtureModel model = gmm.fit(dataset);
+	//	GaussianMixtureModel model = gmm.fit(dataset);
 
 		/*// Output the parameters of the mixture model
 		for (int i = 0; i < model.getK(); i++) {
@@ -73,7 +72,9 @@ public class ClusterManager {
 		          i, model.weights()[i], model.gaussians()[i].mean(), model.gaussians()[i].cov());
 		}*/
 		
-		return model.getK().length();
+		//return model.getK().length();
+		
+		return counter;
 	}
 	
 	public int LDDA(SparkSession spark, String datasetUrl) {
--- ApacheSparkPOC/src/main/java/com/ApacheSparkPOC/DataSizeManager.java
+++ ApacheSparkPOC/src/main/java/com/ApacheSparkPOC/DataSizeManager.java
@@ -9,12 +9,18 @@ public class DataSizeManager {
 	//This method will return the size in bites
 	public long CalculateDataSize(SparkSession spark, String datasetUrl){
 		
+		long size= 0;
+		
+		String format = "libsvm";
+		
 		Dataset<Row> dataset = spark.read().format(format).load(datasetUrl);
 		
 		
-		for(int i= 0, i< dataset.column(); i++) {
+		//for(int i= 0, i< dataset.column(); i++) {
 			
-		}
+		//}		
+		
+		return size;
 		
 	}
 }
